9ad02477aafc602cc300e301a26e16eeaf3b4d85
YuriHyuuga <tiagosouzahiuga2066@gmail.com>
2024-07-24 20:42:13 -0400

    Cenarios

---
+++ TemperatureConverter.java
@@ -0,0 +1,14 @@
+public class TemperatureConverter {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0 / 9.0;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("32°F to Celsius: " + fahrenheitToCelsius(32));
+        System.out.println("0°C to Fahrenheit: " + celsiusToFahrenheit(0));
+    }
+}
